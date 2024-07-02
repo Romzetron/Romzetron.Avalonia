95e8ca54add4346625b0b50765aec121cc326e91
Adam Romzek <romzetron@gmail.com>
2024-07-02 16:25:38 -0400

    Added C# color palette.

---
+++ Romzetron.Avalonia/ColorPalette.cs
@@ -0,0 +1,231 @@
+using Avalonia.Media;
+
+namespace Romzetron.Avalonia;
+
+public static class ColorPalette
+{
+    //==================================================
+    // RED color palette.
+    //==================================================
+
+    public static SolidColorBrush Red00 => SolidColorBrush.Parse("#FFEBEE");
+    public static SolidColorBrush Red01 => SolidColorBrush.Parse("#FFCDD2");
+    public static SolidColorBrush Red02 => SolidColorBrush.Parse("#EF9A9A");
+    public static SolidColorBrush Red03 => SolidColorBrush.Parse("#E57373");
+    public static SolidColorBrush Red04 => SolidColorBrush.Parse("#EF5350");
+    public static SolidColorBrush Red05 => SolidColorBrush.Parse("#F44336");
+    public static SolidColorBrush Red06 => SolidColorBrush.Parse("#E53935");
+    public static SolidColorBrush Red07 => SolidColorBrush.Parse("#D32F2F");
+    public static SolidColorBrush Red08 => SolidColorBrush.Parse("#C62828");
+    public static SolidColorBrush Red09 => SolidColorBrush.Parse("#B71C1C");
+
+    //==================================================
+    // PINK color palette.
+    //==================================================
+
+    public static SolidColorBrush Pink00 => SolidColorBrush.Parse("#FCE4EC");
+    public static SolidColorBrush Pink01 => SolidColorBrush.Parse("#F8BBD0");
+    public static SolidColorBrush Pink02 => SolidColorBrush.Parse("#F48FB1");
+    public static SolidColorBrush Pink03 => SolidColorBrush.Parse("#F06292");
+    public static SolidColorBrush Pink04 => SolidColorBrush.Parse("#EC407A");
+    public static SolidColorBrush Pink05 => SolidColorBrush.Parse("#E91E63");
+    public static SolidColorBrush Pink06 => SolidColorBrush.Parse("#D81B60");
+    public static SolidColorBrush Pink07 => SolidColorBrush.Parse("#C2185B");
+    public static SolidColorBrush Pink08 => SolidColorBrush.Parse("#AD1457");
+    public static SolidColorBrush Pink09 => SolidColorBrush.Parse("#880E4F");
+
+    //==================================================
+    // PURPLE color palette.
+    //==================================================
+
+    public static SolidColorBrush Purple00 => SolidColorBrush.Parse("#EDE7F6");
+    public static SolidColorBrush Purple01 => SolidColorBrush.Parse("#D1C4E9");
+    public static SolidColorBrush Purple02 => SolidColorBrush.Parse("#B39DDB");
+    public static SolidColorBrush Purple03 => SolidColorBrush.Parse("#9575CD");
+    public static SolidColorBrush Purple04 => SolidColorBrush.Parse("#7E57C2");
+    public static SolidColorBrush Purple05 => SolidColorBrush.Parse("#673AB7");
+    public static SolidColorBrush Purple06 => SolidColorBrush.Parse("#5E35B1");
+    public static SolidColorBrush Purple07 => SolidColorBrush.Parse("#512DA8");
+    public static SolidColorBrush Purple08 => SolidColorBrush.Parse("#4527A0");
+    public static SolidColorBrush Purple09 => SolidColorBrush.Parse("#311B92");
+
+    //==================================================
+    // DEEP-PURPLE color palette.
+    //==================================================
+
+    public static SolidColorBrush DeepPurple00 => SolidColorBrush.Parse("#EDE7F6");
+    public static SolidColorBrush DeepPurple01 => SolidColorBrush.Parse("#D1C4E9");
+    public static SolidColorBrush DeepPurple02 => SolidColorBrush.Parse("#B39DDB");
+    public static SolidColorBrush DeepPurple03 => SolidColorBrush.Parse("#9575CD");
+    public static SolidColorBrush DeepPurple04 => SolidColorBrush.Parse("#7E57C2");
+    public static SolidColorBrush DeepPurple05 => SolidColorBrush.Parse("#673AB7");
+    public static SolidColorBrush DeepPurple06 => SolidColorBrush.Parse("#5E35B1");
+    public static SolidColorBrush DeepPurple07 => SolidColorBrush.Parse("#512DA8");
+    public static SolidColorBrush DeepPurple08 => SolidColorBrush.Parse("#4527A0");
+    public static SolidColorBrush DeepPurple09 => SolidColorBrush.Parse("#311B92");
+
+    //==================================================
+    // INDIGO color palette.
+    //==================================================
+
+    public static SolidColorBrush Indigo00 => SolidColorBrush.Parse("#E8EAF6");
+    public static SolidColorBrush Indigo01 => SolidColorBrush.Parse("#C5CAE9");
+    public static SolidColorBrush Indigo02 => SolidColorBrush.Parse("#9FA8DA");
+    public static SolidColorBrush Indigo03 => SolidColorBrush.Parse("#7986CB");
+    public static SolidColorBrush Indigo04 => SolidColorBrush.Parse("#5C6BC0");
+    public static SolidColorBrush Indigo05 => SolidColorBrush.Parse("#3F51B5");
+    public static SolidColorBrush Indigo06 => SolidColorBrush.Parse("#3949AB");
+    public static SolidColorBrush Indigo07 => SolidColorBrush.Parse("#303F9F");
+    public static SolidColorBrush Indigo08 => SolidColorBrush.Parse("#283593");
+    public static SolidColorBrush Indigo09 => SolidColorBrush.Parse("#1A237E");
+
+    //==================================================
+    // BLUE color palette.
+    //==================================================
+
+    public static SolidColorBrush Blue00 => SolidColorBrush.Parse("#E3F2FD");
+    public static SolidColorBrush Blue01 => SolidColorBrush.Parse("#BBDEFB");
+    public static SolidColorBrush Blue02 => SolidColorBrush.Parse("#90CAF9");
+    public static SolidColorBrush Blue03 => SolidColorBrush.Parse("#64B5F6");
+    public static SolidColorBrush Blue04 => SolidColorBrush.Parse("#42A5F5");
+    public static SolidColorBrush Blue05 => SolidColorBrush.Parse("#2196F3");
+    public static SolidColorBrush Blue06 => SolidColorBrush.Parse("#1E88E5");
+    public static SolidColorBrush Blue07 => SolidColorBrush.Parse("#1976D2");
+    public static SolidColorBrush Blue08 => SolidColorBrush.Parse("#1565C0");
+    public static SolidColorBrush Blue09 => SolidColorBrush.Parse("#0D47A1");
+
+    //==================================================
+    // TEAL color palette.
+    //==================================================
+
+    public static SolidColorBrush Teal00 => SolidColorBrush.Parse("#E0F2F1");
+    public static SolidColorBrush Teal01 => SolidColorBrush.Parse("#B2DFDB");
+    public static SolidColorBrush Teal02 => SolidColorBrush.Parse("#80CBC4");
+    public static SolidColorBrush Teal03 => SolidColorBrush.Parse("#4DB6AC");
+    public static SolidColorBrush Teal04 => SolidColorBrush.Parse("#26A69A");
+    public static SolidColorBrush Teal05 => SolidColorBrush.Parse("#009688");
+    public static SolidColorBrush Teal06 => SolidColorBrush.Parse("#00897B");
+    public static SolidColorBrush Teal07 => SolidColorBrush.Parse("#00796B");
+    public static SolidColorBrush Teal08 => SolidColorBrush.Parse("#00695C");
+    public static SolidColorBrush Teal09 => SolidColorBrush.Parse("#004D40");
+
+    //==================================================
+    // GREEN color palette.
+    //==================================================
+
+    public static SolidColorBrush Green00 => SolidColorBrush.Parse("#E8F5E9");
+    public static SolidColorBrush Green01 => SolidColorBrush.Parse("#C8E6C9");
+    public static SolidColorBrush Green02 => SolidColorBrush.Parse("#A5D6A7");
+    public static SolidColorBrush Green03 => SolidColorBrush.Parse("#81C784");
+    public static SolidColorBrush Green04 => SolidColorBrush.Parse("#66BB6A");
+    public static SolidColorBrush Green05 => SolidColorBrush.Parse("#4CAF50");
+    public static SolidColorBrush Green06 => SolidColorBrush.Parse("#43A047");
+    public static SolidColorBrush Green07 => SolidColorBrush.Parse("#388E3C");
+    public static SolidColorBrush Green08 => SolidColorBrush.Parse("#2E7D32");
+    public static SolidColorBrush Green09 => SolidColorBrush.Parse("#1B5E20");
+
+    //==================================================
+    // YELLOW color palette.
+    //==================================================
+
+    public static SolidColorBrush Yellow00 => SolidColorBrush.Parse("#FFFDE7");
+    public static SolidColorBrush Yellow01 => SolidColorBrush.Parse("#FFF9C4");
+    public static SolidColorBrush Yellow02 => SolidColorBrush.Parse("#FFF59D");
+    public static SolidColorBrush Yellow03 => SolidColorBrush.Parse("#FFF176");
+    public static SolidColorBrush Yellow04 => SolidColorBrush.Parse("#FFEE58");
+    public static SolidColorBrush Yellow05 => SolidColorBrush.Parse("#FFEB3B");
+    public static SolidColorBrush Yellow06 => SolidColorBrush.Parse("#FDD835");
+    public static SolidColorBrush Yellow07 => SolidColorBrush.Parse("#FBC02D");
+    public static SolidColorBrush Yellow08 => SolidColorBrush.Parse("#F9A825");
+    public static SolidColorBrush Yellow09 => SolidColorBrush.Parse("#F57F17");
+
+    //==================================================
+    // AMBER color palette.
+    //==================================================
+
+    public static SolidColorBrush Amber00 => SolidColorBrush.Parse("#FFF8E1");
+    public static SolidColorBrush Amber01 => SolidColorBrush.Parse("#FFECB3");
+    public static SolidColorBrush Amber02 => SolidColorBrush.Parse("#FFE082");
+    public static SolidColorBrush Amber03 => SolidColorBrush.Parse("#FFD54F");
+    public static SolidColorBrush Amber04 => SolidColorBrush.Parse("#FFCA28");
+    public static SolidColorBrush Amber05 => SolidColorBrush.Parse("#FFC107");
+    public static SolidColorBrush Amber06 => SolidColorBrush.Parse("#FFB300");
+    public static SolidColorBrush Amber07 => SolidColorBrush.Parse("#FFA000");
+    public static SolidColorBrush Amber08 => SolidColorBrush.Parse("#FF8F00");
+    public static SolidColorBrush Amber09 => SolidColorBrush.Parse("#FF6F00");
+
+    //==================================================
+    // ORANGE color palette.
+    //==================================================
+
+    public static SolidColorBrush Orange00 => SolidColorBrush.Parse("#FFF3E0");
+    public static SolidColorBrush Orange01 => SolidColorBrush.Parse("#FFE0B2");
+    public static SolidColorBrush Orange02 => SolidColorBrush.Parse("#FFCC80");
+    public static SolidColorBrush Orange03 => SolidColorBrush.Parse("#FFB74D");
+    public static SolidColorBrush Orange04 => SolidColorBrush.Parse("#FFA726");
+    public static SolidColorBrush Orange05 => SolidColorBrush.Parse("#FF9800");
+    public static SolidColorBrush Orange06 => SolidColorBrush.Parse("#FB8C00");
+    public static SolidColorBrush Orange07 => SolidColorBrush.Parse("#F57C00");
+    public static SolidColorBrush Orange08 => SolidColorBrush.Parse("#EF6C00");
+    public static SolidColorBrush Orange09 => SolidColorBrush.Parse("#E65100");
+
+    //==================================================
+    // DEEP-ORANGE color palette.
+    //==================================================
+
+    public static SolidColorBrush DeepOrange00 => SolidColorBrush.Parse("#FBE9E7");
+    public static SolidColorBrush DeepOrange01 => SolidColorBrush.Parse("#FFCCBC");
+    public static SolidColorBrush DeepOrange02 => SolidColorBrush.Parse("#FFAB91");
+    public static SolidColorBrush DeepOrange03 => SolidColorBrush.Parse("#FF8A65");
+    public static SolidColorBrush DeepOrange04 => SolidColorBrush.Parse("#FF7043");
+    public static SolidColorBrush DeepOrange05 => SolidColorBrush.Parse("#FF5722");
+    public static SolidColorBrush DeepOrange06 => SolidColorBrush.Parse("#F4511E");
+    public static SolidColorBrush DeepOrange07 => SolidColorBrush.Parse("#E64A19");
+    public static SolidColorBrush DeepOrange08 => SolidColorBrush.Parse("#D84315");
+    public static SolidColorBrush DeepOrange09 => SolidColorBrush.Parse("#BF360C");
+
+    //==================================================
+    // BROWN color palette.
+    //==================================================
+
+    public static SolidColorBrush Brown00 => SolidColorBrush.Parse("#EFEBE9");
+    public static SolidColorBrush Brown01 => SolidColorBrush.Parse("#D7CCC8");
+    public static SolidColorBrush Brown02 => SolidColorBrush.Parse("#BCAAA4");
+    public static SolidColorBrush Brown03 => SolidColorBrush.Parse("#A1887F");
+    public static SolidColorBrush Brown04 => SolidColorBrush.Parse("#8D6E63");
+    public static SolidColorBrush Brown05 => SolidColorBrush.Parse("#795548");
+    public static SolidColorBrush Brown06 => SolidColorBrush.Parse("#6D4C41");
+    public static SolidColorBrush Brown07 => SolidColorBrush.Parse("#5D4037");
+    public static SolidColorBrush Brown08 => SolidColorBrush.Parse("#4E342E");
+    public static SolidColorBrush Brown09 => SolidColorBrush.Parse("#3E2723");
+
+    //==================================================
+    // GREY color palette.
+    //==================================================
+
+    public static SolidColorBrush Grey00 => SolidColorBrush.Parse("#FAFAFA");
+    public static SolidColorBrush Grey01 => SolidColorBrush.Parse("#BDBDBD");
+    public static SolidColorBrush Grey02 => SolidColorBrush.Parse("#A7A7A7");
+    public static SolidColorBrush Grey03 => SolidColorBrush.Parse("#929292");
+    public static SolidColorBrush Grey04 => SolidColorBrush.Parse("#7E7E7E");
+    public static SolidColorBrush Grey05 => SolidColorBrush.Parse("#6A6A6A");
+    public static SolidColorBrush Grey06 => SolidColorBrush.Parse("#565656");
+    public static SolidColorBrush Grey07 => SolidColorBrush.Parse("#444444");
+    public static SolidColorBrush Grey08 => SolidColorBrush.Parse("#323232");
+    public static SolidColorBrush Grey09 => SolidColorBrush.Parse("#212121");
+
+    //==================================================
+    // BLUE-GREY color palette.
+    //==================================================
+
+    public static SolidColorBrush BlueGrey00 => SolidColorBrush.Parse("#ECEFF1");
+    public static SolidColorBrush BlueGrey01 => SolidColorBrush.Parse("#CFD8DC");
+    public static SolidColorBrush BlueGrey02 => SolidColorBrush.Parse("#B0BEC5");
+    public static SolidColorBrush BlueGrey03 => SolidColorBrush.Parse("#90A4AE");
+    public static SolidColorBrush BlueGrey04 => SolidColorBrush.Parse("#78909C");
+    public static SolidColorBrush BlueGrey05 => SolidColorBrush.Parse("#607D8B");
+    public static SolidColorBrush BlueGrey06 => SolidColorBrush.Parse("#546E7A");
+    public static SolidColorBrush BlueGrey07 => SolidColorBrush.Parse("#455A64");
+    public static SolidColorBrush BlueGrey08 => SolidColorBrush.Parse("#37474F");
+    public static SolidColorBrush BlueGrey09 => SolidColorBrush.Parse("#263238");
+}
