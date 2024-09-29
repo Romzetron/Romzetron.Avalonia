eae5e7b3da1ee53f7433924b45b97fd985fa28b1
Adam Romzek <romzetron@gmail.com>
2024-09-29 12:06:22 -0400

    Added better example for NotificationCard that includes center postitions.

--- Romzetron.Avalonia.Example/Views/Tabs/TabNotificationCard.axaml.cs
+++ Romzetron.Avalonia.Example/Views/Tabs/TabNotificationCard.axaml.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Notifications;
@@ -5,7 +7,7 @@ using Avalonia.Interactivity;
 
 namespace Romzetron.Avalonia.Example.Views.Tabs;
 
-public partial class TabNotificationCard : UserControl
+public partial class TabNotificationCard : UserControl, INotifyPropertyChanged
 {
     //==================================================
     // Private variables.
@@ -13,8 +15,32 @@ public partial class TabNotificationCard : UserControl
 
     private WindowNotificationManager? _notificationManagerTopRight;
     private WindowNotificationManager? _notificationManagerTopLeft;
+    private WindowNotificationManager? _notificationManagerTopCenter;
     private WindowNotificationManager? _notificationManagerBottomRight;
     private WindowNotificationManager? _notificationManagerBottomLeft;
+    private WindowNotificationManager? _notificationManagerBottomCenter;
+
+    //==================================================
+    // Private property variables.
+    //==================================================
+
+    private string _selectedNotificationType = "Information";
+
+    //==================================================
+    // Properties.
+    //==================================================
+
+    public ObservableCollection<string> NotificationTypes { get; } = ["Information", "Success", "Warning", "Error"];
+
+    public string SelectedNotificationType
+    {
+        get => _selectedNotificationType;
+        set
+        {
+            _selectedNotificationType = value;
+            OnPropertyChanged(nameof(SelectedNotificationType));
+        }
+    }
 
     //==================================================
     // Constructor.
@@ -23,6 +49,7 @@ public partial class TabNotificationCard : UserControl
     public TabNotificationCard()
     {
         InitializeComponent();
+        DataContext = this;
     }
 
     //==================================================
@@ -36,8 +63,10 @@ public partial class TabNotificationCard : UserControl
 
         _notificationManagerTopRight = new WindowNotificationManager(topLevel) { Position = NotificationPosition.TopRight };
         _notificationManagerTopLeft = new WindowNotificationManager(topLevel) { Position = NotificationPosition.TopLeft };
+        _notificationManagerTopCenter = new WindowNotificationManager(topLevel) { Position = NotificationPosition.TopCenter };
         _notificationManagerBottomRight = new WindowNotificationManager(topLevel) { Position = NotificationPosition.BottomRight };
         _notificationManagerBottomLeft = new WindowNotificationManager(topLevel) { Position = NotificationPosition.BottomLeft };
+        _notificationManagerBottomCenter = new WindowNotificationManager(topLevel) { Position = NotificationPosition.BottomCenter };
     }
 
     //==================================================
@@ -47,7 +76,7 @@ public partial class TabNotificationCard : UserControl
 
     private void ShowTopRightNotification_OnClick(object? sender, RoutedEventArgs e)
     {
-        _notificationManagerTopRight?.Show(new Notification("Notification", "This is a top right Information notification."));
+        _notificationManagerTopRight?.Show(new Notification("Notification", $"This is a top right {SelectedNotificationType} notification."), GetNotificationType());
     }
 
     //==================================================
@@ -57,7 +86,17 @@ public partial class TabNotificationCard : UserControl
 
     private void ShowTopLeftNotification_OnClick(object? sender, RoutedEventArgs e)
     {
-        _notificationManagerTopLeft?.Show(new Notification("Notification", "This is a top left Success notification.", NotificationType.Success));
+        _notificationManagerTopLeft?.Show(new Notification("Notification", $"This is a top left {SelectedNotificationType} notification.", GetNotificationType()));
+    }
+
+    //==================================================
+    // Display success notification in top center of the
+    // window.
+    //==================================================
+
+    private void ShowTopCenterNotification_OnClick(object? sender, RoutedEventArgs e)
+    {
+        _notificationManagerTopCenter?.Show(new Notification("Notification", $"This is a top center {SelectedNotificationType} notification."), GetNotificationType());
     }
 
     //==================================================
@@ -67,7 +106,7 @@ public partial class TabNotificationCard : UserControl
 
     private void ShowBottomLeftNotification_OnClick(object? sender, RoutedEventArgs e)
     {
-        _notificationManagerBottomLeft?.Show(new Notification("Notification", "This is a bottom left Warning notification.", NotificationType.Warning));
+        _notificationManagerBottomLeft?.Show(new Notification("Notification", $"This is a bottom left {SelectedNotificationType} notification.", GetNotificationType()));
     }
 
     //==================================================
@@ -77,6 +116,43 @@ public partial class TabNotificationCard : UserControl
 
     private void ShowBottomRightNotification_OnClick(object? sender, RoutedEventArgs e)
     {
-        _notificationManagerBottomRight?.Show(new Notification("Notification", "This is a bottom right Error notification.", NotificationType.Error));
+        _notificationManagerBottomRight?.Show(new Notification("Notification", $"This is a bottom right {SelectedNotificationType} notification.", GetNotificationType()));
+    }
+
+    //==================================================
+    // Display success notification in bottom center of
+    // the window.
+    //==================================================
+
+    private void ShowBottomCenterNotification_OnClick(object? sender, RoutedEventArgs e)
+    {
+        _notificationManagerBottomCenter?.Show(new Notification("Notification", $"This is a bottom center {SelectedNotificationType} notification."), GetNotificationType());
+    }
+
+    //==================================================
+    // Convert notification type string to enum.
+    //==================================================
+
+    private NotificationType GetNotificationType()
+    {
+        return SelectedNotificationType switch
+        {
+            "Information" => NotificationType.Information,
+            "Success" => NotificationType.Success,
+            "Warning" => NotificationType.Warning,
+            "Error" => NotificationType.Error,
+            _ => NotificationType.Information
+        };
+    }
+
+    //==================================================
+    // Property changed event handling.
+    //==================================================
+
+    public new event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
